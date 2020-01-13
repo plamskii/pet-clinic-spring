133241fae5c742d6c5703d9b85875e8003c8f54c
karaspla <plamenk@starsgroup.com>
2020-01-13 16:38:18 +0200

    git ignor

---
+++ src/main/java/spring/framework/petclinicsfg/PetClinicSfgApplication.java
@@ -0,0 +1,13 @@
+package spring.framework.petclinicsfg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinicSfgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetClinicSfgApplication.class, args);
+    }
+
+}
---
+++ src/test/java/spring/framework/petclinicsfg/PetClinicSfgApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.framework.petclinicsfg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicSfgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
